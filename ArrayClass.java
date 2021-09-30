7884b07ed7b990ae57ee80f66ff5fba62a7cea6e
KumaresanTester <91654181+KumaresanTester@users.noreply.github.com>
2021-09-30 12:15:34 +0530

    Add files via upload

---
+++ ArrayClass.java
@@ -0,0 +1,16 @@
+package array.packages;
+
+public class ArrayClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] a= {1,2,3,4};
+		
+		for (int numbersList : a) {
+			System.out.println(numbersList);
+			
+		}
+
+	}
+
+}
